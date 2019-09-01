408d90636eadfb90ab835fa88b26ff1386c59d27
rahuldshetty <35rahuldshetty@gmail.com>
2019-09-01 20:41:56 +0530

    login register without database

--- app/src/main/java/com/rahuldshetty/socialconnect/activities/LoginActivity.java
+++ app/src/main/java/com/rahuldshetty/socialconnect/activities/LoginActivity.java
@@ -1,19 +1,36 @@
 package com.rahuldshetty.socialconnect.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.rahuldshetty.socialconnect.MainActivity;
 import com.rahuldshetty.socialconnect.R;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageView loginBtn;
     private EditText emailFld, passFld;
-    private TextView forgotPass, signIn;
+    private TextView forgotPass, signUp;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +41,54 @@ public class LoginActivity extends AppCompatActivity {
         emailFld = findViewById(R.id.login_email);
         passFld = findViewById(R.id.login_password);
         forgotPass = findViewById(R.id.login_forgot);
-        signIn = findViewById(R.id.reg_login);
+        signUp = findViewById(R.id.reg_login);
+        progressBar = findViewById(R.id.logProgressBar);
+
+        mAuth =  FirebaseAuth.getInstance();
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(act);
+                finish();
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email = emailFld.getText().toString();
+                String pass = passFld.getText().toString();
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pass)){
+
+                    mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                progressBar.setVisibility(View.INVISIBLE);
+                                Intent act = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(act);
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+
+                        }
+                    });
+
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Enter all the Fields.",Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/rahuldshetty/socialconnect/activities/RegisterActivity.java
+++ app/src/main/java/com/rahuldshetty/socialconnect/activities/RegisterActivity.java
@@ -1,16 +1,127 @@
 package com.rahuldshetty.socialconnect.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.rahuldshetty.socialconnect.MainActivity;
 import com.rahuldshetty.socialconnect.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText nameFld,emailFld,passFld,cpassFld;
+    private TextView signIn;
+    private ImageView regBtn;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        nameFld = findViewById(R.id.reg_name);
+        emailFld = findViewById(R.id.reg_email);
+        passFld = findViewById(R.id.reg_password);
+        cpassFld = findViewById(R.id.reg_password2);
+        signIn = findViewById(R.id.reg_signin);
+        regBtn = findViewById(R.id.reg_imageView);
+        progressBar = findViewById(R.id.regProgressBar);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent act = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(act);
+                finish();
+            }
+        });
+
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                String name = nameFld.getText().toString();
+                String email = emailFld.getText().toString();
+                String pass = passFld.getText().toString();
+                String cpass = cpassFld.getText().toString();
+
+                String msg = validate(name,email,pass,cpass);
+
+                if(msg.equals("")){
+                    // password valid
+
+                    //user sign up
+                    mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                // sign up completed...
+                                // add info to database
+                                goToMain();
+                            }
+                            else
+                            {
+                                Toast.makeText(RegisterActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this,msg,Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
     }
+
+    void goToMain(){
+        Intent main = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(main);
+        finish();
+
+    }
+
+    String validate(String name,String email,String pass,String cpass){
+        if(TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(cpass) ){
+            return "Enter all the fields.";
+        }
+        else if(!pass.equals(cpass)){
+            return "Password Mismatch.";
+        }
+        else if(!pass.matches(".*[a-z].*"))
+            return "Password should contain at least one lower case letter.";
+        else if(!pass.matches(".*[A-Z].*"))
+            return "Password should contain at least one upper case letter.";
+        else if(!pass.matches(".*[0-9].*"))
+            return "Password should contain at least one digit.";
+        else if(!pass.matches(".*[^a-zA-Z0-9].*"))
+            return "Password should contain at least one special symbol.";
+        return "";
+    }
+
 }
