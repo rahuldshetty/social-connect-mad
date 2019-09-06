1539df5c6a0259433dac51d3de570a347581323a
rahuldshetty <35rahuldshetty@gmail.com>
2019-09-06 19:37:55 +0530

    added search bar(not feature yet)

--- app/src/main/java/com/rahuldshetty/socialconnect/fragments/SearchFragment.java
+++ app/src/main/java/com/rahuldshetty/socialconnect/fragments/SearchFragment.java
@@ -4,17 +4,30 @@ package com.rahuldshetty.socialconnect.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.rahuldshetty.socialconnect.MainActivity;
 import com.rahuldshetty.socialconnect.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private SearchView searchView;
+    private View view;
+
+    private FirebaseFirestore db;
+    private FirebaseStorage storage;
 
 
     public SearchFragment() {
@@ -26,7 +39,30 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        view =  inflater.inflate(R.layout.fragment_search, container, false);
+
+        recyclerView = view.findViewById(R.id.search_recycler_view);
+        searchView = view.findViewById(R.id.search_view);
+
+        db =  FirebaseFirestore.getInstance().collection("USERS").getFirestore();
+        storage = FirebaseStorage.getInstance();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+        return view;
     }
+
 
 }
